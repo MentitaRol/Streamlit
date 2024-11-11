5b0d48c9570e1b8b71b7d1a2bd15a245006a6fa5
Fernanda Valdez <142357001+MentitaRol@users.noreply.github.com>
2024-11-11 07:56:20 -0600

    Add files via upload

---
+++ database.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
+df = pd.read_csv("Police1.csv")
+
+st.markdown('The data shown below belongs to incident reports in the city of San Francisco,from the year 2018 to 2020, with details from each case suach as date, day of the week, police district, neighborhood in wi')
+
+mapa = pd.DataFrame()
+
+mapa['Date'] = df['Incident Date']
+mapa['Day'] = df['Incident Day of the Week']
+mapa['Police District'] = df['Police District']
+mapa['Neighborhood'] = df['Analysis Neighborhood']
+mapa['Incident Category'] = df['Incident Category']
+mapa['Incident Subcategory'] = df['Incident Subcategory']
+mapa['Resolution'] = df['Resolution']
+mapa['lat'] = df['Latitude']
+mapa['lon'] = df['Longitude']
+mapa = mapa.dropna()
+
+subset_data2 = mapa
+police_district_input = st.sidebar.multiselect(
+    'Police District',
+    mapa.groupby('Police District').count().reset_index()['Police District'].tolist())
+if len(police_district_input) > 0:
+    subset_data2 = mapa[mapa['Police District'].isin(police_district_input)]
